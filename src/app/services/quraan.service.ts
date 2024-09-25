3f08a767af21d6ac27fa7c27a4041da65c0bc8b8
ma9da <dodymasry25@gmail.com>
2024-09-25 21:42:31 +0300

    added surah meta data

--- src/app/services/quraan.service.ts
+++ src/app/services/quraan.service.ts
@@ -9,11 +9,15 @@ export class QuraanService {
   baseUrl: string = `https://api.alquran.cloud/v1`;
   quraanEndPoint: string = `/quran/quran-uthmani`;
   surahEndPoint: string = `/surah`;
+  pageEndPoint: string = `/page/`;
   constructor(private http: HttpClient) {}
   getQuraan(): Observable<any> {
     return this.http.get(`${this.baseUrl}${this.quraanEndPoint}`);
   }
   getSurah(number: number): Observable<any> {
     return this.http.get(`${this.baseUrl}${this.surahEndPoint}/${number}`);
+  }
+  getPage(number: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}${this.pageEndPoint}/${number}`);
   }
 }
