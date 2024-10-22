0391cca9f9d030139ec525c39f670a1e3200503f
ma9da <dodymasry25@gmail.com>
2024-10-23 02:26:39 +0300

    fix imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoaderComponent } from './components/loader/loader.component';
@@ -7,7 +8,14 @@ import { LoaderComponent } from './components/loader/loader.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, LoaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    LoaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
